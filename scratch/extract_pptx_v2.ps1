64fcab5facdedbb4da69780829cbe87455ce4808
baguswiratama48-ai <baguswiratama48@guru.sma.belajar.id>
2026-04-12 05:46:07 +0700

    chore: tambah temp_pptx dan scratch ke .gitignore

--- scratch/extract_pptx_v2.ps1
+++
@@ -1,6 +0,0 @@
-$slides = Get-ChildItem "temp_pptx\ppt\slides\slide*.xml" | Sort-Object { [int]($_.BaseName -replace 'slide', '') }
-foreach ($slide in $slides) {
-    echo "`n--- $($slide.BaseName) ---"
-    [xml]$xml = Get-Content $slide.FullName
-    echo $xml.InnerText
-}
